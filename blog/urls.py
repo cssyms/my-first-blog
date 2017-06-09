1087ea85b37edf3f8515b140d305ab3ceb69e675
cssyms <cssyms@nate.com>
2017-06-09 15:55:56 +0900

    change the HTML for site  2serp.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
 
+# CSyang
 urlpatterns = [
 	url(r'^$', views.post_list, name='post_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
-
+# CSyang
 def post_list(request):
 	return render(request, 'blog/post_list.html', {})
 	
